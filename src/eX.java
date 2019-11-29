131c0ccfc2fb6d399a25d2716361c79cf441a9d3
Alumne <Alumne@104-PC12>
2019-11-29 16:56:09 +0100

    SegonCommit

--- src/eX.java
+++ src/eX.java
@@ -8,16 +8,32 @@ public class eX {
 	
 	@SuppressWarnings("rawtypes")
 	public static void main (String[] args) {
+		Scanner teclado = new Scanner(System.in);
 		
 		try {
+			
+			//Demanem la URL
+			System.out.println("Introdueix una URL: exemple -> http://insbaixcamp.org/index.php/2013-10-29-12-24-12/informacio-general-4 <-");
+			String urlinsti = teclado.nextLine();
+			
+			//Demanem els camps de capçalera que es volen
+			System.out.println("Introdueix els camps de capçalera que vols: exemple -> 1 , 2 , 3 , 4 <-");
+			int campCapçalera = teclado.nextInt();
+			
+			//Demanem el text per mostrar arguments
+			System.out.println("Introdueix un text per mostrar arguments, exemple: ->  #java cerca  http://insbaixcamp.org/  5  script <-");
+			String textarguments = teclado.nextLine();
+			
 			String cadena;
-			URL url = new URL("http://insbaixcamp.org/index.php/2013-10-29-12-24-12/informacio-general-4");
+			URL url = new URL(urlinsti);
 			URLConnection connexio = url.openConnection();
 			
+			//Mostrem la data i contingut
 			System.out.println("===============================================================");
 			System.out.println("ADREÇA, DARA I CONTINGUT");
 			System.out.println("Adreça [getURL]: " + connexio.getURL());
 			
+			//Mostrem la ultima data de modificacio
 			Date data = new Date(connexio.getLastModified());
 			System.out.println("Data última modificació [getLastModified()]: " + data);
 			System.out.println("Tipus de Contingut [getContentType()]" + connexio.getContentType());
@@ -34,10 +50,12 @@ public class eX {
 				System.out.println(map.getKey() + ":" + map.getValue());
 			}
 			
+			//Mostrem la quantitat de camps de capçelera que ens ha demanat el usuari
 			System.out.println("===============================================================");
-			System.out.println("Camps 1 i 4 de Capçalera");
-			System.out.println("getHeaderField(1)=> " + connexio.getHeaderField(1));
-			System.out.println("getHeaderField(4)=> " + connexio.getHeaderField(4));
+			System.out.println("Camps de Capçalera");
+			for (int i = 0; i < campCapçalera; i++) {
+				System.out.println("getHeaderField(" + i + ")=>"  + connexio.getHeaderField(i));
+			}
 			System.out.println("===============================================================");
 			
 			System.out.println("Contingut de [url.getFile()]: " + url.getFile());
@@ -61,7 +79,7 @@ public class eX {
 		 String headerType = con.getContentType();
 		 String guessType = con.guessContentTypeFromName(url.
 		getFile());
-		 ret = headerType.endsWith("gif") || guessType.endsWith("gif");
+		 ret = headerType.endsWith("script") || guessType.endsWith("script");
 		 } catch (IOException ex) {
 		 Logger.getLogger(eX.class.getName()).log(Level.
 		SEVERE, null, ex);
